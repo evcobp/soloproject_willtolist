af6ec2098e8658f3a316a89b75b752062d328bf0
Brandon <brandon@evco.ventures>
2020-08-26 00:25:22 -0500

    initial commit to final build

--- willto_app/models.py
+++ willto_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.datastructures import MultiValueDictKeyError
+import datetime
+from datetime import date
 import re
 import bcrypt
 
@@ -7,33 +10,45 @@ EMAIL_MATCH = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 # Create your models here.
 class UserManager(models.Manager):
     #Get User emails
-    def get_all_username(self):
-        return self.order_by('username')
+    def get_all_by_email(self):
+        return self.order_by('email')
     
     #New User Registration
-    def register(self, postData):
-        myhash = bcrypt.hashpw(postData['password'].encode(), bcrypt.gensalt()).decode()
-        User.objects.create(
-            first_name= postData['first_name'],
-            last_name= postData['last_name'],
-            username= postData['username'],
-            email= postData['email'],
+    def register(self, form_data):
+        myhash = bcrypt.hashpw(form_data['password'].encode(), bcrypt.gensalt()).decode()
+        return self.create(
+            first_name= form_data['first_name'],
+            last_name= form_data['last_name'],
+            username= form_data['username'],
+            email= form_data['email'],
             password= myhash
             )
         
-    def authenticate(self, email):
+    def authenticate(self, email, password):
         users_with_email = self.filter(email=email)
         if not users_with_email:
             return False
         user = users_with_email[0]
         return bcrypt.checkpw(password.encode(), user.password.encode())
         
-    def basic_validator(self, postData):
+    def validate(self, postData):
+        
         errors = {}
         if len(postData['first_name']) < 1:
             errors["first_name"] = "Field cannot be blank."
         if len(postData['last_name']) < 1:
             errors["last_name"] = "Field cannot be blank."
+        if len(postData['email']) < 1:
+            errors["email"] = "Email cannot be blank."
+        if not EMAIL_MATCH.match(postData['email']):
+            errors['email'] = "Invalid Email."
+        if postData['password'] != postData['confirm_password']:
+            errors['password'] = "Passwords do not match"
+        
+        users_with_email = self.filter(email=postData['email'])
+        if users_with_email:
+            errors['email'] = "Email already in use."
+        
         return errors
             
     
@@ -44,11 +59,14 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
     confirm_password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = UserManager()
     
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.username} {self.email}"
     
-    objects = UserManager()
     
 class Task(models.Model):
     task_name = models.TextField()
@@ -61,6 +79,8 @@ class Task(models.Model):
     question_four = models.IntegerField(default = 1, validators=[MaxValueValidator(10), MinValueValidator(1)])
     question_five = models.IntegerField(default = 1, validators=[MaxValueValidator(10), MinValueValidator(1)])
     score = 'question_one'+'question_two'+'question_three'+'question_four'+'question_five'
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return f"{self.task_name} {self.notes} {self.score}"
--- willto_app/urls.py
+++ willto_app/urls.py
@@ -1,12 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
 from . import views
 
 
 urlpatterns = [
-    path('', views.index),
-    path('registration', views.registration),
-    path('login', views.login),
-    path('logout', views.logout,),
+    path('', views.login_page, name="login_page"),
+    path('registration', views.registration, name="registration"),
+    path('registration_page', views.registration_page, name="registration_page"),
+    path('login', views.login, name="login"),
+    path('logout', views.logout, name="logout"),
     path('success', views.success),
     path('create_user', views.create_user),
     path('create_task', views.create_task),
@@ -14,4 +16,9 @@ urlpatterns = [
     path('task_questions', views.task_questions),
     path('new_task', views.new_task),
     path('score', views.score),
+    url(r'^save-session-data/$', views.save_session_data, name='save_session_data'),
+    url(r'^access-session-data/$', views.access_session_data, name='access_session_data'),
+    url(r'^delete-session-data/$', views.delete_session_data, name='delete_session_data'),
+    url(r'^test-delete/$', views.test_delete, name='test_delete'),
+    url(r'^test-session/$', views.test_session, name='test_session'),
 ]
